716cf93cbdc48f26fe083ea52c9c0985aed5fd33
harshsbhat <harsh121102@gmail.com>
2024-03-27 13:00:02 +0530

    Frontend added

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from search_buddy.search import main
 
 app = Flask(__name__)
 
-@app.route('/search', methods=['POST'])  # Change method to POST
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/search', methods=['POST'])
 def search():
-    data = request.json  # Get data from request body
+    data = request.form  # Get data from form submission
     keywords = data.get('keywords')
-    array = data.get('array')
+    array = data.getlist('array')
 
     if keywords is None or array is None:
         return jsonify({'error': 'Keywords and array parameters are required.'}), 400
 
     similar_array = main(keywords, array)
-    return jsonify({'similar_array': similar_array})
+    return render_template('results.html', similar_array=similar_array)
 
 if __name__ == '__main__':
     app.run(debug=True)
